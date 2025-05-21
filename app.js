1a1994019a6d1009d87d49a75e14072e8d75800a
amradhwen <radhweneamri9631@gmail.com>
2025-05-22 00:15:33 +0100

    project initialization

---
+++ app.js
@@ -0,0 +1,22 @@
+require ("dotenv").config();
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+
+
+const app = express();
+const PORT = 5000 || process.env.PORT;
+
+// static (stylesheets, images, scripts...etc)
+app.use(express.static("public"))
+
+// Template Engine
+app.use(expressLayouts);
+app.set("layout", "./layouts/main");
+app.set("view-engine", "ejs");
+
+// routes
+app.use("/", require("./server/routes/main"));
+
+app.listen(PORT, ()=>{
+	console.log(`Server Listening on port ${PORT}`)
+})
